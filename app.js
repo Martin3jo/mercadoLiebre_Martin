4e806be12bb780d9f5e64a40afb1c982a2d0584e
CoronelAtilio <coronelatiliomaximiliano@gmail.com>
2023-10-25 14:31:27 -0300

    update antes de la clase

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+let port = 3002;
+
+app.get('/home',(req,res)=>{
+	res.sendFile(path.join(__dirname,'./views/home.html'));
+});
+
+app.get('/',(req,res)=>{
+	res.send('hola estas en about')
+});
+
+app.listen(port, ()=>{
+    console.log(`
+    Nuestra app funciona en
+    http://localhost:${port}`);
+})
+
+app.use(express.static('public'));
